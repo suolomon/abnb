8bd19a4624a954cf1b914b0ed98aed55f7f36ce2
Solomon Talemwa <adamtblack96@gmail.com>
2021-08-05 09:36:39 +0300

    add changes

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Image from 'next/Image'
+
+const Header = () => {
+    return (
+        <Header>
+           <div>
+               <Image
+               src="https://links.papareact.com/qd3"
+               layout="fil"/>
+           </div>
+
+           <div></div>
+
+           <div></div>
+        </Header>
+    )
+}
+
+export default Header
