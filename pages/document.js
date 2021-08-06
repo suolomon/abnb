c301eb888bd9e891f6b47d5c8a2ebc9a9973ba6c
Solomon Talemwa <adamtblack96@gmail.com>
2021-08-06 17:06:32 +0300

    add document with class object

--- pages/document.js
+++ pages/document.js
@@ -1,23 +1,24 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-function MyDocument() {
-  Document();
-  return (
-    <Html lang="en">
-      {/* <Html lang='en' class='dark'> */}
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-black-300 dark:to-black-700 dark:text-white">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        {/* <Html lang='en' class='dark'> */}
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-black-300 dark:to-black-700 dark:text-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
